91abb3ebbabdf787cb0177c7e168c500b3165a30
Lakshyajeet Jalal <lakshyajeetjalal@gmail.com>
2025-04-01 21:51:44 +0530

    ast printer fix

--- com/craftinginterpreters/lox/AstPrinter.java
+++ com/craftinginterpreters/lox/AstPrinter.java
@@ -1,64 +1,69 @@
 package com.craftinginterpreters.lox;
 
-// class AstPrinter implements Expr.Visitor<String> {
-// String print(Expr expr) {
-// return expr.accept(this);
-// }
+class AstPrinter implements Expr.Visitor<String> {
+    String print(Expr expr) {
+        return expr.accept(this);
+    }
 
-// @Override
-// public String visitBinaryExpr(Expr.Binary expr) {
-// return parenthesize(expr.operator.lexeme, expr.left, expr.right);
-// }
+    @Override
+    public String visitBinaryExpr(Expr.Binary expr) {
+        return parenthesize(expr.operator.lexeme, expr.left, expr.right);
+    }
 
-// @Override
-// public String visitGroupingExpr(Expr.Grouping expr) {
-// return parenthesize("group", expr.expression);
-// }
+    @Override
+    public String visitLogicalExpr(Expr.Logical expr) {
+        return parenthesize(expr.operator.lexeme, expr.left, expr.right);
+    }
 
-// @Override
-// public String visitLiteralExpr(Expr.Literal expr) {
-// if (expr.value == null)
-// return "nil";
-// return expr.value.toString();
-// }
+    @Override
+    public String visitGroupingExpr(Expr.Grouping expr) {
+        return parenthesize("group", expr.expression);
+    }
 
-// @Override
-// public String visitUnaryExpr(Expr.Unary expr) {
-// return parenthesize(expr.operator.lexeme, expr.right);
-// }
+    @Override
+    public String visitLiteralExpr(Expr.Literal expr) {
+        if (expr.value == null)
+            return "nil";
+        return expr.value.toString();
+    }
 
-// @Override
-// public String visitAssignExpr(Expr.Assign expr) {
-// return parenthesize("= " + expr.name.lexeme, expr.value);
-// }
+    @Override
+    public String visitUnaryExpr(Expr.Unary expr) {
+        return parenthesize(expr.operator.lexeme, expr.right);
+    }
 
-// @Override
-// public String visitVariableExpr(Expr.Variable expr) {
-// return parenthesize("var " + expr.name);
-// }
+    @Override
+    public String visitAssignExpr(Expr.Assign expr) {
+        return parenthesize("= " + expr.name.lexeme, expr.value);
+    }
 
-// private String parenthesize(String name, Expr... expers) {
-// StringBuilder builder = new StringBuilder();
+    @Override
+    public String visitVariableExpr(Expr.Variable expr) {
+        return parenthesize("var " + expr.name);
+    }
 
-// builder.append("(").append(name);
-// for (Expr expr : expers) {
-// builder.append(" ");
-// builder.append(expr.accept(this));
-// }
-// builder.append(")");
+    private String parenthesize(String name, Expr... expers) {
+        StringBuilder builder = new StringBuilder();
 
-// return builder.toString();
-// }
+        builder.append("(").append(name);
+        for (Expr expr : expers) {
+            builder.append(" ");
+            builder.append(expr.accept(this));
+        }
+        builder.append(")");
 
-// // public static void main(String[] args) {
-// // Expr expression = new Expr.Binary(
-// // new Expr.Unary(
-// // new Token(TokenType.MINUS, "-", null, 1),
-// // new Expr.Literal(123)),
-// // new Token(TokenType.STAR, "*", null, 1),
-// // new Expr.Grouping(
-// // new Expr.Literal(45.67)));
+        return builder.toString();
+    }
 
-// // System.out.println(new AstPrinter().print(expression));
-// // }
-// }
+    // public static void main(String[] args) {
+    // Expr expression = new Expr.Binary(
+    // new Expr.Unary(
+    // new Token(TokenType.MINUS, "-", null, 1),
+    // new Expr.Literal(123)),
+    // new Token(TokenType.STAR, "*", null, 1),
+    // new Expr.Grouping(
+    // new Expr.Literal(45.67)));
+
+    // System.out.println(new AstPrinter().print(expression));
+    // }
+}
